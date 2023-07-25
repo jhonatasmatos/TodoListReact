fb7813e99947fe48eab56789901cfa8fa1169dc7
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-07-25 14:24:13 +0100

    feat: Adding create, check and delete task

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { ChangeEvent, useMemo, useState } from 'react'
+import { v4 } from 'uuid'
 
 import { Header } from './components/Header'
 
@@ -7,12 +8,52 @@ import { PlusCircle, Trash, Check, ClipboardText } from 'phosphor-react'
 import './global.css'
 import styles from './App.module.css'
 
+interface Task {
+  id: string
+  completed: boolean
+  content: string
+}
+
 export function App() {
-  const [isChecked, setIsChecked] = useState(true)
-  const [tasks, setTasks] = useState([])
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [newTask, setNewTask] = useState('')
 
+  function handleCreateTask() {
 
-  console.log(tasks.length)
+    const addedTask = {
+      id: v4(),
+      completed: false,
+      content: newTask
+    }
+
+    setTasks([...tasks, addedTask])
+    setNewTask('')
+  }
+
+  function handleNewCommentChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTask(event?.target.value)
+  }
+
+  function handleDeleteTask(taskId: string) {
+    const tasksWithoutDeletedOne = tasks.filter(task => {
+        return task.id !== taskId
+    })
+
+    setTasks(tasksWithoutDeletedOne)
+  }
+
+  function handleChangeStatus(taskId: string) {
+    const taskToEdit = tasks.find(task => task.id === taskId)
+
+    if(taskToEdit) {
+      taskToEdit.completed = !taskToEdit.completed
+    }
+
+    const editedTasks = [...tasks]
+    setTasks(editedTasks)
+  }
+
+  const tasksDone = useMemo(() => tasks.filter(task => task.completed).length, [tasks])
 
   return (
     <div>
@@ -24,9 +65,11 @@ export function App() {
               type="text" 
               name="inputTask" 
               id="inputTask"
+              value={newTask}
+              onChange={handleNewCommentChange}
               placeholder='Adicione uma nova tarefa'
             />
-            <button className={styles.button} title="Criar">
+            <button onClick={handleCreateTask} className={styles.button} title="Criar">
               Criar
               <PlusCircle size={16} weight='bold' />
             </button>
@@ -36,11 +79,19 @@ export function App() {
             <div className={styles.statusTasksWrapper}>
               <div className={styles.statusWrapper}>
                 <p>Tarefas criadas</p>
-                <span>5</span>
+                <span>{tasks.length}</span>
               </div>
               <div className={styles.statusWrapper}>
                 <p>Concluídas</p>
-                <span>2 de 5</span>
+                <span>
+                  {
+                    tasks.length === 0 ? (
+                      0
+                    ): (
+                      <>{tasksDone} de {tasks.length}</>
+                    )
+                  }
+                </span>
               </div>
             </div>
             {
@@ -51,18 +102,23 @@ export function App() {
                   <p>Crie tarefas e organize seus itens a fazer</p>
                 </div>
               ): (
-                <div className={styles.taskWrapper}>
-                  <button className={isChecked ? styles.checked : styles.unchecked}>
-                    <Check weight='bold' />
-                  </button>
-                  <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
-                  <button type='button' title='Trash'>
-                    <Trash size={20} weight='bold' />
-                  </button>
-                </div>
+                <>
+                  {
+                    tasks.map(task => (
+                      <div key={task.id} className={styles.taskWrapper}>
+                        <button onClick={() => handleChangeStatus(task.id)} className={task.completed ? styles.checked : styles.unchecked}>
+                          <Check weight='bold' />
+                        </button>
+                        <p className={task.completed ? styles.taskComplete : styles.taskIncomplete}>{task.content}</p>
+                        <button onClick={() => handleDeleteTask(task.id)} type='button' title='Trash'>
+                          <Trash size={20} weight='bold' />
+                        </button>
+                      </div>
+                    ))
+                  }
+                </>
               )
             }
-          {/* </div> */}
       </main>
     </div>
   )
