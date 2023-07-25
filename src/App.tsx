7831d8ba9b59ac31ab74c1e36e7c8eb9c89d4ff0
jhonatasmatos <jhonatas.santos.matos@gmail.com>
2023-07-25 02:00:10 +0100

    feat: Creating basic html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,39 @@
+import { Header } from "./components/Header"
 
-function App() {
+import './global.css'
+
+export function App() {
 
   return (
-    <h1>Hello, React</h1>
+    <div>
+      <Header />
+      <main>
+          <div>
+            <input type="text" name="inputTask" id="inputTask" />
+            <button title="Criar">
+              Criar
+              x
+            </button>
+          </div>
+          <div>
+            <div>
+              <p>Tarefas criadas</p>
+              <span>5</span>
+            </div>
+            <div>
+              <p>Concluídas</p>
+              <span>2 de 5</span>
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>x</p>
+              <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+              <p>x</p>
+            </div>
+          </div>
+      </main>
+    </div>
   )
 }
 
